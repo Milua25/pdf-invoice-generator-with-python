28b3c10a73d21f4973196577435a752e208c2fda
Ayo-milua <ademiluaayo94@gmail.com>
2025-05-22 01:28:26 -0400

    updated the invoice generator scripts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import pandas as pd
 from glob import glob
 from fpdf import  FPDF
@@ -9,8 +9,12 @@ for invoice in invoice_list:
     df = pd.read_excel(invoice, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", format="A4", unit="mm")
     pdf.add_page()
-    filename = (os.path.basename(invoice)).split("-")[0]
-    print(filename)
+    filename, date = Path(invoice).stem.split("-")
     pdf.set_font("Times", size=12, style="B")
-    pdf.cell(200, 10, txt="Invoice nr." + filename, align="C")
+    pdf.cell(50, 8, txt="Invoice nr." + filename, ln=1)
+
+    pdf.set_font("Times", size=12, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+
+
     pdf.output("./PDFs/PDF_" + filename + ".pdf")
